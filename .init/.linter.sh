fb57060f5bb20408248f24e93bfb8971d2efd262
Kavia AI <production@kavia.ai>
2025-08-19 11:09:15 +0000

    CheckPoint - cg230bd176

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/event-ticketing-platform-127324-127333/event_ticketing_frontend
+flutter analyze
+
---
+++ event_ticketing_frontend/android/app/src/main/kotlin/com/example/event_ticketing_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.event_ticketing_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
